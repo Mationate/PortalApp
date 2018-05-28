ae0826c65f7dd75067b9287bdf2d25278cdfa59c
Matias Onate Fraga <mationate.0@gmail.com>
2018-05-28 16:01:52 -0400

    Post in Github !

--- app/src/main/java/com/mationate/portalapp/LoginActivity.java
+++ app/src/main/java/com/mationate/portalapp/LoginActivity.java
@@ -14,6 +14,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
+
         Button loginBtnFake = findViewById(R.id.loginBtnFake);
         loginBtnFake.setOnClickListener(new View.OnClickListener() {
             @Override
